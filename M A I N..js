1a756de0dda29db04339c1183449928e541bf5eb
YouLookedAtMyUsername <tali.k.gai@gmail.com>
2024-05-03 17:56:21 -0400

    Maybe Working

--- M A I N..js
+++ M A I N..js
@@ -1,66 +1,74 @@
 function ChangePixelColor(W, H, R, G, B) {
-    let CellName = `Cell${W}${H}`
-    let Color = `rgb(${R}, ${G}, ${B})`
+    let CellName = `Cell${W}${H}`;
+    let Color = `rgb(${R}, ${G}, ${B})`;
     document.getElementById(CellName).style.backgroundColor = Color;
 }
 
 const Cells = document.querySelectorAll('.Cell');
-console.log("Cells")
+console.log("tgtgtgtyhfxdftcghjbgyjgvlsowpw");
 
 Cells.forEach(Cell => {
     Cell.addEventListener("click", function() {
-        let R = document.getElementById("RTEXTBOX").value
-        let G = document.getElementById("GTEXTBOX").value
-        let B = document.getElementById("BTEXTBOX").value
-        let Color = `rgb(${R}, ${G}, ${B})`
-        Cell.style.backgroundColor = Color
-    }, true)
-})
+        let R = document.getElementById("RTEXTBOX").value;
+        let G = document.getElementById("GTEXTBOX").value;
+        let B = document.getElementById("BTEXTBOX").value;
+        let Color = `rgb(${R}, ${G}, ${B})`;
+        Cell.style.backgroundColor = Color;
+    }, true);
+});
 
 document.getElementById("APPLY").addEventListener("click", function() {
-    let W = document.getElementById("WTEXTBOX").value
-    let H = document.getElementById("HTEXTBOX").value
-    let R = document.getElementById("RTEXTBOX").value
-    let G = document.getElementById("GTEXTBOX").value
-    let B = document.getElementById("BTEXTBOX").value
-    ChangePixelColor(W-1, H-1, R, G, B)
+    let W = document.getElementById("WTEXTBOX").value;
+    let H = document.getElementById("HTEXTBOX").value;
+    let R = document.getElementById("RTEXTBOX").value;
+    let G = document.getElementById("GTEXTBOX").value;
+    let B = document.getElementById("BTEXTBOX").value;
+    if(W > 10 || W < 1) {
+        alert("Width is not valid. 1-10");
+        return;
+    }
+    if(H > 10 || H < 1) {
+        alert("Height is not valid. 1-10");
+        return;
+    }
+    ChangePixelColor(W-1, H-1, R, G, B);
 })
 
 function GetPixel(Column, Row) {
-    let CellName = `Cell${Column}${Row}`
+    let CellName = `Cell${Column}${Row}`;
     var rgb = window.getComputedStyle(document.getElementById(CellName)).backgroundColor;
     rgb = rgb.replace(/[^\d,]/g, '').split(',');
-    return `{${rgb[0]}, ${rgb[1]}, ${rgb[2]}}`
+    return `{${rgb[0]}, ${rgb[1]}, ${rgb[2]}}`;
 }
 
 function GetRow(Row) {
-    let CONSOLELOG = "{"
-    CONSOLELOG += GetPixel(0, Row) + ","
-    CONSOLELOG += GetPixel(1, Row) + ","
-    CONSOLELOG += GetPixel(2, Row) + ","
-    CONSOLELOG += GetPixel(3, Row) + ","
-    CONSOLELOG += GetPixel(4, Row) + ","
-    CONSOLELOG += GetPixel(5, Row) + ","
-    CONSOLELOG += GetPixel(6, Row) + ","
-    CONSOLELOG += GetPixel(7, Row) + ","
-    CONSOLELOG += GetPixel(8, Row) + ","
-    CONSOLELOG += GetPixel(9, Row) + "}"
-    return CONSOLELOG
+    let CONSOLELOG = "{";
+    CONSOLELOG += GetPixel(0, Row) + ",";
+    CONSOLELOG += GetPixel(1, Row) + ",";
+    CONSOLELOG += GetPixel(2, Row) + ",";
+    CONSOLELOG += GetPixel(3, Row) + ",";
+    CONSOLELOG += GetPixel(4, Row) + ",";
+    CONSOLELOG += GetPixel(5, Row) + ",";
+    CONSOLELOG += GetPixel(6, Row) + ",";
+    CONSOLELOG += GetPixel(7, Row) + ",";
+    CONSOLELOG += GetPixel(8, Row) + ",";
+    CONSOLELOG += GetPixel(9, Row) + "}";
+    return CONSOLELOG;
 }
 
 document.getElementById("TABLEINATOR").addEventListener("click", function() {
-    let LOG = "{"
-    LOG += GetRow(0) + ","
-    LOG += GetRow(1) + ","
-    LOG += GetRow(2) + ","
-    LOG += GetRow(3) + ","
-    LOG += GetRow(4) + ","
-    LOG += GetRow(5) + ","
-    LOG += GetRow(6) + ","
-    LOG += GetRow(7) + ","
-    LOG += GetRow(8) + ","
-    LOG += GetRow(9) + "};"
-    console.log(LOG)
-    navigator.clipboard.writeText(LOG)
-    alert("Copied")
+    let LOG = "{";
+    LOG += GetRow(0) + ",";
+    LOG += GetRow(1) + ",";
+    LOG += GetRow(2) + ",";
+    LOG += GetRow(3) + ",";
+    LOG += GetRow(4) + ",";
+    LOG += GetRow(5) + ",";
+    LOG += GetRow(6) + ",";
+    LOG += GetRow(7) + ",";
+    LOG += GetRow(8) + ",";
+    LOG += GetRow(9) + "};";
+    console.log(LOG);
+    navigator.clipboard.writeText(LOG);
+    alert("Copied");
 })
